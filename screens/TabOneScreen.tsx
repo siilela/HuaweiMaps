22cb12c151c95f8db57e9bdc537b630b65671cdb
Siiela Mogashoa <sumogashoa@shoprite.co.za>
2022-12-13 14:51:41 +0200

    commit test for Huawei Maps

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,38 @@
 import { StyleSheet } from 'react-native';
 
+import React, {  useState, useEffect } from 'react';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
-
+import {
+  ErrorCode,
+  HMSAvailability,
+} from '@hmscore/react-native-hms-availability';
+import HMSMap, { MapTypes } from '@hmscore/react-native-hms-map'; 
+ //For MapTypes when are you going to update the package Typo from MapType to MapTypes on the Docs?
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [isHauweiDevice, setIsHuaweiDevice] = useState<boolean>(false);
+  const checkIfHuaweiDevice = async () => {
+    const huaweiResults =
+    //This is the HMS availability error am getting .
+      await HMSAvailability.isHuaweiMobileServicesAvailable();
+      setIsHuaweiDevice(huaweiResults === ErrorCode.HMS_CORE_APK_AVAILABLE);
+  };
+
+  useEffect(() => {
+      checkIfHuaweiDevice();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Huawei Map One </Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <HMSMap
+        mapType={MapTypes.NORMAL}
+        style={{ height: 400 }}
+        camera={{ target: { latitude: 41, longitude: 29 }, zoom: 11 }}
+      />
     </View>
   );
 }
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,14 +1,37 @@
 import { StyleSheet } from 'react-native';
 
+import React, { useState, useEffect } from 'react';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+import {
+  ErrorCode,
+  HMSAvailability,
+} from '@hmscore/react-native-hms-availability';
+ //For MapTypes when are you going to update the package Typo from MapType to MapTypes on the Docs.
+import HMSMap, { MapTypes } from '@hmscore/react-native-hms-map'; 
+
 export default function TabTwoScreen() {
+  const [isHauweiDevice, setIsHuaweiDevice] = useState<boolean>(false);
+  const checkIfHuaweiDevice = async () => {
+    const huaweiResults =
+    //This is the HMS availability error am getting .
+      await HMSAvailability.isHuaweiMobileServicesAvailable();
+      setIsHuaweiDevice(huaweiResults === ErrorCode.HMS_CORE_APK_AVAILABLE);
+  };
+
+  useEffect(() => {
+      checkIfHuaweiDevice();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Huawei Map Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <HMSMap
+        mapType={MapTypes.NORMAL}
+        style={{ height: 400 }}
+        camera={{ target: { latitude: 41, longitude: 29 }, zoom: 11 }}
+      />
     </View>
   );
 }
